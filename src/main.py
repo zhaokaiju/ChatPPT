ddb013513208e6302b0add2d65ac16374b34d0c2
ziyucoding <zhaokaiju@foxmail.com>
2025-08-10 15:15:25 +0800

    [MOD]增加练习代码。

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from input_parser import parse_input_text
 from ppt_generator import generate_presentation
 from template_manager import load_template, get_layout_mapping, print_layouts
 
+
 def main():
     input_text = """
     # ChatPPT_Demo
@@ -14,15 +15,16 @@ def main():
     - 市场份额扩大至30%
 
     ## 业绩图表 [Title and Picture 1]
-    ![业绩图表](images/performance_chart.png)
+    ![业绩图表](../images/performance_chart.png)
 
     ## 新产品发布 [Title and 2 Column]
     - 产品A: 特色功能介绍
     - 产品B: 市场定位
-    ![未来增长](images/forecast.png)
+    ![未来增长](../images/forecast.png)
     """
 
-    template_file = 'templates/MasterTemplate.pptx'
+    # template_file = '../templates/MasterTemplate.pptx'
+    template_file = '../templates/Fair frames presentation.pptx'
     prs = load_template(template_file)
 
     print("Available Slide Layouts:")
@@ -32,8 +34,9 @@ def main():
 
     powerpoint_data, presentation_title = parse_input_text(input_text, layout_mapping)
 
-    output_pptx = f"outputs/{presentation_title}.pptx"
+    output_pptx = f"../outputs/{presentation_title}.pptx"
     generate_presentation(powerpoint_data, template_file, output_pptx)
+
 
 if __name__ == "__main__":
     main()
